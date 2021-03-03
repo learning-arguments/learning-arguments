fdb83f86d8b648916825f3e798d9bf0b20097650
luedman <Lukas@LUKASDESKTOP>
2021-03-03 01:59:35 +0100

    propositions are replaced by facts, coherence, conclusiveness

--- logic.py
+++ logic.py
@@ -3,101 +3,158 @@ from itertools import combinations
 import collections
 
 
-class Proposition:
-    def __init__(self, interpretation: bool, name: str):
-        self._interpretation = interpretation
-        self._name = name
-
-    def __bool__(self):
-        return self._interpretation
+class Fact:
+    def __init__(self, statement: str, not_negated: bool = True):
+        self._statement = statement if not_negated else 'not %s' % statement
+        self._not_negated = not_negated
 
     def __eq__(self, other):
-        return self._interpretation == bool(other)
+        return (self._statement == other.statement) and (self._not_negated == other.not_negated)
 
     def __hash__(self):
-        return hash((self.name, self.interpretation))
+        return hash((self.statement, self._not_negated))
+
+    def __bool__(self):
+        return self._not_negated
 
     @property
-    def interpretation(self) -> bool:
-        return self._interpretation
+    def statement(self) -> str:
+        return self._statement
 
     @property
-    def name(self) -> str:
-        return self._name
+    def not_negated(self) -> bool:
+        return self._not_negated
 
 
-class Negation(Proposition):
-    def __init__(self, a: Proposition):
-        interpretation: bool = not a.interpretation
-        super().__init__(interpretation, a.name)
+class Negation(Fact):
+    def __init__(self, a: Fact):
+        truth_value: bool = not a.not_negated
+        statement = 'not %s' % a.statement
+        super().__init__(statement, truth_value)
 
 
-class And(Proposition):
-    def __init__(self, a: Proposition, b: Proposition):
-        interpretation: bool = True if a and b else False
-        super().__init__(interpretation, '%s_and_%s' % (a.name, b.name))
+class And(Fact):
+    def __init__(self, a: Fact, b: Fact):
+        truth_value: bool = True if a and b else False
+        statement = '%s and %s' % (a.statement, b.statement)
+        super().__init__(statement, truth_value)
 
 
-class Or(Proposition):
-    def __init__(self, a: Proposition, b: Proposition):
-        interpretation: bool = True if a or b else False
-        super().__init__(interpretation, '%s_or_%s' % (a.name, b.name))
+class Or(Fact):
+    def __init__(self, a: Fact, b: Fact):
+        truth_value: bool = True if a or b else False
+        statement = '%s or %s' % (a.statement, b.statement)
+        super().__init__(statement, truth_value)
 
 
-class Equivalence(Proposition):
-    def __init__(self, a: Proposition, b: Proposition):
-        interpretation: bool = (a == b)
-        super().__init__(interpretation, '%s_eq_%s' % (a.name, b.name))
+class Equivalence(Fact):
+    def __init__(self, a: Fact, b: Fact):
+        truth_value: bool = (a == b)
+        statement = '%s is equivalent to %s' % (a.statement, b.statement)
+        super().__init__(statement, truth_value)
 
 
-class Tautology(Proposition):
-    def __init__(self, a_list=List[Proposition]):
-        interpretation: bool = any(a_list)  # Not sure whether this is correct
-        super().__init__(interpretation, 'tautology')
+class Tautology(Fact):
+    def __init__(self, a_list=List[Fact]):
+        truth_value: bool = any(a_list)  # Not sure whether this is correct
+        statement = 'tautology'
+        super().__init__(statement, truth_value)
 
 
-class Case(Proposition):
-    def __init__(self, fact_set: List[Proposition], value: int, name: str):
+class Argument:
+    # An argument from 'P to not Q' makes the case 'P and not Q'
+    # see Definition 2, page 132
+    def __init__(self, premise: Fact, conclusion: Fact):
+        self._premise = premise
+        self._conclusion = conclusion
+
+    def __str__(self):
+        return 'if %s then %s' % (self._premise.statement, self._conclusion.statement)
+
+    @property
+    def conclusion(self):
+        return self._conclusion
+
+    @property
+    def premise(self):
+        return self._premise
+
+
+class Case:
+    def __init__(self, fact_set: List[Fact], probability=0.0, name: str = None):
         self._fact_set = fact_set
-        self._value = value
-        interpretation = all(self._fact_set)
-        super().__init__(interpretation, name)
+        self._value = probability
+        self._name = name
 
     @property
     def fact_set(self):
         return self._fact_set
 
     @property
-    def value(self):
+    def probability(self):
         return self._value
+
+    @property
+    def name(self):
+        return self._name
 
     def __eq__(self, other):
         return collections.Counter(self._fact_set) == collections.Counter(other.fact_set)
 
+    def __add__(self, other):
+        return Case(self._fact_set + other.fact_set,
+                    self.probability + other.probability,
+                    self.name + ' and ' + other.name)
+
+    def negate(self):
+        negated_fact_set = [Fact(f.statement, not f.not_negated) for f in self._fact_set]
+        return Case(fact_set=negated_fact_set, probability=self.probability, name=self.name)
+
 
 class CaseModel:
     def __init__(self, cases: List[Case]):
-        self._cases: List[Case] = cases
+        self._case_list: List[Case] = cases
         self._valid = False
         self._check_validity()
 
     def _check_validity(self):
         # Definition 1, page 131
-        # ToDo: Implement condition 1 and 5
-        for case1, case2 in combinations(self._cases, 2):
-            if not Equivalence(case1, case2):
-                assert Negation(And(case1, case2)), "Case Model not valid, condition 2 was violated"
-            if Equivalence(case1, case2):
+        # ToDo: Implement condition 5
+        for case1, case2 in combinations(self._case_list, 2):
+            assert case1.negate() not in self._case_list, "Case Model not valid, condition 1 was violated"
+            if not case1 == case2:
+                assert not case1 + case2 in self._case_list, "Case Model not valid, condition 2 was violated"
+            if case1 == case2:
                 assert case1 == case2, "Case Model not valid, condition 3 was violated"
-            if not (case1.value >= case2.value or case1.value <= case2.value):
+                # This is trivial
+            if not (case1.probability >= case2.probability or case1.probability <= case2.probability):
                 assert False, "Case Model not valid, condition 4 was violated"
 
         self._valid = True
 
     @property
     def cases(self):
-        return self._cases
+        return self._case_list
 
     @property
     def valid(self):
         return self._valid
+
+    def coherent(self, arg: Argument):
+        for case in self._case_list:
+            if arg.premise in case.fact_set and arg.conclusion in case.fact_set and case.probability > 0.0:
+                return True
+        return False
+
+    def conclusive(self, arg):
+        if self.coherent(arg):
+            for case in self._case_list:
+                if not (case.probability > 0.0 and arg.premise in case.fact_set and arg.conclusion in case.fact_set):
+                    return False
+                else:
+                    return True
+
+
+def conditional_probability(case1: Case, case2: Case):
+    if case2.probability != 0.0:
+        return (case1.probability + case2.probability) / case2.probability
--- testing.py
+++ testing.py
@@ -3,30 +3,43 @@ import unittest
 
 
 def test_1():
-    rain = Proposition(True, 'rain')
-    sun = Proposition(True, 'sun')
+    rain = Fact('rain')
+    sun = Fact('sun')
+    not_sun = Fact('sun', False)
 
-    case1 = Case([Negation(sun), rain], value=1, name='case1')
-    case2 = Case([sun, Negation(rain)], value=2, name='case2')
+    case1 = Case([not_sun, rain], probability=0.5, name='case1')
+    case2 = Case([sun], probability=0.5, name='case2')
 
     case_model = CaseModel(cases=[case1, case2])
+
+    argument = Argument(premise=rain, conclusion=not_sun)
+    print('%s is coherent: %s' % (argument, case_model.coherent(argument)))
+    print('%s is conclusive: %s' % (argument, case_model.conclusive(argument)))
+
     return case_model.valid
 
 
 def test_simonshaven():
-    guilty = Proposition(True, 'guilty')
-    robbery = Proposition(True, 'robbery')
-    perry = Proposition(True, 'Perry')
-    third = Proposition(True, 'third')
-    all_evidence = Proposition(True, 'all evidence')
+    guilty = Fact('guilty')
+    robbery = Fact('robbery')
+    perry = Fact('Perry')
+    third = Fact('third')
+    all_evidence = Fact('all evidence')
 
-    case1 = Case([guilty, all_evidence], value=0.5, name='case 1')
-    case2_a = Case([Negation(guilty), robbery, perry, Negation(all_evidence)], value=0.2, name='case 2_a')
-    case2_b = Case([Negation(guilty), robbery, Negation(perry), third, Negation(all_evidence)], value=0.2, name='case2_b')
-    case3 = Case([Negation(guilty), Negation(robbery), Negation(all_evidence)], value=0.1, name='case 3')
+    not_guilty = Fact('guilty', False)
+    not_robbery = Fact('robbery', False)
+    not_perry = Fact('Perry', False)
+    not_all_evidence = Fact('all evidence', False)
+
+    case1 = Case([guilty, all_evidence], probability=0.5, name='case 1')
+    case2_a = Case([guilty, robbery, perry, not_all_evidence], probability=0.2, name='case 2_a')
+    case2_b = Case([not_guilty, robbery, not_perry, third, not_all_evidence], probability=0.2,
+                   name='case2_b')
+    case3 = Case([not_guilty, not_robbery, not_all_evidence], probability=0.1, name='case 3')
 
     case_model = CaseModel(cases=[case1, case2_a, case2_b, case3])
     return case_model.valid
+
 
 class TestStringMethods(unittest.TestCase):
     # https://docs.python.org/3/library/unittest.html

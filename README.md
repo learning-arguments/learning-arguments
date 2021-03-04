9da85feb8590638ef2bfdfd3c7bf22cad59d7bee
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-03-04 20:59:38 +0100

    Refactor tests to use `pytest` as testing framework

---
+++ README.md
@@ -0,0 +1,9 @@
+# Explainable AI: Learning Arguments
+
+## Testing
+
+```
+pytest
+```
+
+(This requires [pytest](https://docs.pytest.org/en/stable/getting-started.html), which can be installed with `pip install -U pytest`.)
--- logic_test.py
+++ logic_test.py
@@ -1,5 +1,4 @@
 from logic import *
-import unittest
 
 
 def test_1():
@@ -16,7 +15,7 @@ def test_1():
     print('%s is coherent: %s' % (argument, case_model.coherent(argument)))
     print('%s is conclusive: %s' % (argument, case_model.conclusive(argument)))
 
-    return case_model.valid
+    assert case_model.valid
 
 
 def test_simonshaven():
@@ -32,24 +31,12 @@ def test_simonshaven():
     not_all_evidence = Fact('all evidence', False)
 
     case1 = Case([guilty, all_evidence], probability=0.5, name='case 1')
-    case2_a = Case([guilty, robbery, perry, not_all_evidence], probability=0.2, name='case 2_a')
+    case2_a = Case([guilty, robbery, perry, not_all_evidence],
+                   probability=0.2, name='case 2_a')
     case2_b = Case([not_guilty, robbery, not_perry, third, not_all_evidence], probability=0.2,
                    name='case2_b')
-    case3 = Case([not_guilty, not_robbery, not_all_evidence], probability=0.1, name='case 3')
+    case3 = Case([not_guilty, not_robbery, not_all_evidence],
+                 probability=0.1, name='case 3')
 
     case_model = CaseModel(cases=[case1, case2_a, case2_b, case3])
-    return case_model.valid
-
-
-class TestStringMethods(unittest.TestCase):
-    # https://docs.python.org/3/library/unittest.html
-    # functions in here have to start with 'test'
-    def test_case_model_creation(self):
-        self.assertTrue(test_1())
-
-    def test_case_model_simonshaven(self):
-        self.assertTrue(test_simonshaven())
-
-
-if __name__ == "__main__":
-    unittest.main()
+    assert case_model.valid
